8e60a5f580300048b56dfbaef180ba207507f52e
Janamajaya Mall <janmajaya.choithram@gmail.com>
2021-11-04 15:10:00 +0800

    deplyed new version

--- src/helpers.ts
+++ src/helpers.ts
@@ -2,7 +2,7 @@ import { BigInt, BigDecimal, Address } from "@graphprotocol/graph-ts";
 import { MarketFactory } from "./../generated/MarketFactory/MarketFactory";
 
 export const ADDRESS_ZERO = "0x0000000000000000000000000000000000000000";
-export const FACTORY_ADDRESS = "0x5F2E80E35F64157e769F38392953b60461e5BbAb";
+export const FACTORY_ADDRESS = "0x36beE43e0548cfAa63e8094895b2Ede098e36B46";
 
 export let ZERO_BI = BigInt.fromI32(0);
 export let ONE_BI = BigInt.fromI32(1);
